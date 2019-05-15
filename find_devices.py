bb245c542e6564e04afb8bf415db5f70bbdd39e8
BuildTools <unconfigured@null.spigotmc.org>
2019-05-15 01:39:31 -0300

    add script for finding devices

---
+++ find_devices.py
@@ -0,0 +1,13 @@
+import threespace_api as th
+
+def main():
+    devices = th.getComPorts()
+    print("%d device(s) found" % len(devices))
+    if not len(devices): return
+    
+    for i, device in enumerate(devices):
+        print("device #%d:" % i + 1)
+        print("type: {}".format(device.dev_type))
+        print("port: {}".format(device.com_port))
+
+if __name__ == "__main__": main()
