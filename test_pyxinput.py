62f50057f6cedf8325a8b44c4630fb6ef65bdf2f
Rayner Sulyak de Souza <rayner.sulyak@corp.int.gov.br>
2019-05-15 16:00:05 -0300

    add pynput for keyboard listener

--- test_pyxinput.py
+++ test_pyxinput.py
@@ -1,3 +1,23 @@
 import pyxinput as pyx
+from pynput import keyboard
 
-pyx.test_virtual()
+def main():
+    print("hello world")
+    #pyx.test_virtual()
+
+    def on_press(key):
+        try:
+            print('alphanumeric key {0} pressed'.format(key))
+        except AttributeError:
+            print('special key {0} pressed'.format(key))
+
+    def on_release(key):
+        print('{0} released'.format(key))
+        if key == keyboard.Key.esc:
+            # Stop listener
+            return False
+
+    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+        listener.join()
+
+if __name__ == "__main__": main()
