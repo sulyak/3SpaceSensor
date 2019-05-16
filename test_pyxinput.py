8315058c4726deda8ee048959c13a57f7f11a4b9
Rayner Sulyak de Souza <rayner.sulyak@corp.int.gov.br>
2019-05-16 17:42:26 -0300

    add unity test controller

--- test_pyxinput.py
+++ test_pyxinput.py
@@ -2,12 +2,15 @@ import pyxinput as pyx
 from pynput import keyboard
 
 def main():
+    controller = pyx.vController(1)
     print("hello world")
     #pyx.test_virtual()
 
     def on_press(key):
         try:
             print('alphanumeric key {0} pressed'.format(key))
+            if(key.char == "a"):
+                controller.set_value("BtnA", 1)
         except AttributeError:
             print('special key {0} pressed'.format(key))
 
@@ -16,7 +19,12 @@ def main():
         if key == keyboard.Key.esc:
             # Stop listener
             return False
-
+        try:
+            if(key.char == "a"):
+                controller.set_value("BtnA", 0)
+        except:
+            print("unknown key {}".format(key))
+        
     with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
         listener.join()
 
