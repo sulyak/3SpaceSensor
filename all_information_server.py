82a1af874bc160a3c8c98e1141e44eb5a1da886a
Rayner Sulyak <raynersulyak@gmail.com>
2019-05-21 17:55:54 -0300

    add testing server

---
+++ all_information_server.py
@@ -0,0 +1,60 @@
+from find_devices import display_devices
+
+import api.threespace_api as tss
+import zmq
+
+"""get all data from wired sensor and send to unity
+
+this is a test program to help test the interaction between unity and python
+
+this file is similar to get_all_information.py 
+"""
+
+def main():
+    display_devices()
+
+    wired = []
+    devices = tss.getComPorts()
+    # finding all wired sensors
+    # TODO add compatibility with dongles
+    for device in devices:
+        if device.dev_type == "WL":
+            wired.append(device)
+    
+    # assert that there is a wired sensor to work with
+    if(not len(wired)):
+        print("please use a wired wireless sensor")
+        print("terminating the program...")
+        return
+    
+    # connect with first device found
+    true_device = tss.TSWLSensor(wired[0].com_port)
+    print("\n\nconnecting with device at com port %s..." % wired[0].com_port)
+    print("press any key to continue")
+    input()
+
+    # connect to server
+    PORT = 5555
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    socket.bind("tcp://*" + PORT)
+
+    try:
+        while True:
+            message = socket.recv()
+            print("request received: %s" % message)
+
+            # TODO diffrent answers from diffrent requests
+            if(message == "getAllData"):
+                data = true_device.getAllRawComponentSensorData()
+                out = "[%f, %f, %f] --Gyro\n"\
+                    "[%f, %f, %f] --Accel\n"\
+                    "[%f, %f, %f] --Comp" % data
+                print("sending:\n" + out)
+                socket.send_string(out)
+            else:
+                socket.send_string("unrecognized request")
+    except KeyboardInterrupt:
+        print("stopped")
+            
+if __name__ == "__main__": main()
