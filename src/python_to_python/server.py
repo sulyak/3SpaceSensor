3f6a031aa0f27d0f0621310a2699572fd246b2d9
Rayner <raynersulyak@gmail.com>
2019-05-25 00:04:42 -0300

    add change port

--- src/python_to_python/server.py
+++ src/python_to_python/server.py
@@ -1,19 +1,30 @@
+import sys
 import zmq
 
-PORT = "5551"
-
 def main():
+    port = "5559"
+    if len(sys.argv) > 1:
+        port = sys.argv[1]
+    
     # stabilish server
     context = zmq.Context()
     socket = context.socket(zmq.REP)
-    socket.bind("tcp://*:%s" % PORT)
+    socket.bind("tcp://*:%s" % port)
+
+    print("usage: python %s [port=5559]" % sys.argv[0])
+    print("creating server tcp://127.0.0.1:%s" % port)
 
     while True:
-        request = socket.recv()
-        print("request received: %s " % request)
-        print("what shall be the reply?")
-        reply = input()
-        socket.send_string(reply)
+        try:
+            request = socket.recv()
+            print("request received: %s " % request)
+            print("what shall be the reply?")
+            reply = input()
+            socket.send_string(reply)
+        except KeyboardInterrupt:
+            break
+    
+    socket.close()
 
 
 
