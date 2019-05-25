7ac5f0cf3d90a98178a4f2a11484d983c91902e9
Rayner <raynersulyak@gmail.com>
2019-05-24 21:58:31 -0300

    change port

--- src/python_to_python/client.py
+++ src/python_to_python/client.py
@@ -1,13 +1,13 @@
 import zmq
 
-PORT = "5551"
+PORT = "5559"
 
 def main():
     # connect to server
     context = zmq.Context()
     socket = context.socket(zmq.REQ)
 
-    # couldnt use 'tcp://*:5551' 
+    # couldnt use 'tcp://*:5551' insted of 'tcp://127.0.0.1:5551'
     socket.connect("tcp://127.0.0.1:%s" % PORT)
 
     message = input("send a message:")
