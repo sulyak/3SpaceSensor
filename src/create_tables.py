131352515a16bef4b0284df8588cba028ea3cbc5
Rayner Sulyak de Souza <rayner.sulyak@corp.int.gov.br>
2019-10-16 17:57:13 -0300

    add time

--- src/create_tables.py
+++ src/create_tables.py
@@ -18,7 +18,7 @@ def main():
             dongles.append(device)
     
     # assert that there is a dongle to work with
-    if(not len(dongles)):
+    if not len(dongles):
         print("please use a dongle")
         print("terminating the program...")
         return
@@ -29,16 +29,27 @@ def main():
     print("press any key to continue")
     input()
 
+    # get the wireless sensors paired to the dongle
     true_devices = []
     for true_device in true_dongle:
-        print(true_device)
+        # print(true_device)
         if true_device: true_devices.append(true_device)
 
-    print("using %d devices" % len(true_devices))
+    print("using %d wireless device(s)" % len(true_devices))
     print("press any key to continue...")
-    input()
+    input()   
 
-    generate_table(true_devices, 3, t_len=3)
+    # get the length of the recording in seconds
+    print("how long should the recording be? (3 - 60) in seconds")
+    t_len = input()
+    try: t_len = int(t_len)
+    except: t_len = 3
+    if 3 < t_len > 60: t_len = 3
+    print("recording for %d seconds..." % t_len)
+    time.sleep(2)
+
+    
+    generate_table(true_devices, 3, t_len=t_len)
 
 def generate_table(devices, mode, t_len=10):
     """
@@ -53,18 +64,22 @@ def generate_table(devices, mode, t_len=10):
 
     function_name = ["getRawGyroscopeRate", "getRawAccelerometerData", "getRawCompassData"]
     try:
+        # run the recording for t_len seconds
         t_end = time.time() + t_len
         while time.time() < t_end:
             if mode != 3:
                 line = ""
                 for i, device in enumerate(devices):
+                    # this way we can call a function using its name as a string
                     function = getattr(device, function_name[mode])
+                    # separate the items with an empty column (separating output from diffrent devices)
                     if i: line += ",,"
                     line += "%.03f,%.03f,%.03f" % function()
                 line += ";\n"
                 fp.writelines(line)
                 print(line, end='')
             else:
+                # this is the mode with all 3 other function
                 for j, func_name in enumerate(function_name):
                     line = ""
                     for i, device in enumerate(devices):
@@ -80,10 +95,6 @@ def generate_table(devices, mode, t_len=10):
     except KeyboardInterrupt:
         print("stopped")
 
-
     fp.close()
-
-
-
 
 if __name__ == "__main__": main()
