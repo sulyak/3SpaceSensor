e9a722e710926cd498133dfa2eb0ec532accd6c5
Rayner Sulyak <raynersulyak@gmail.com>
2019-12-06 05:27:29 -0300

    remove spredsheet

--- create_tables.py
+++ create_tables.py
@@ -49,57 +49,29 @@ def main():
     print("recording for %d seconds..." % t_len)
     countdown(3)
     
-    generate_table(true_devices, 3, t_len=t_len)
+    generate_table(true_devices, t_len=t_len)
 
-def generate_table(devices, mode, t_len=10):
-    """
-    0 - gyro
-    1 - accel
-    2 - compass
-    3 - all of the above
-    """
-    if mode not in range(4): return
-    file_name = ["gyro", "accel", "comp", "all_data"]
-    fp = open("table_" + file_name[mode] + ".txt", "w")
-
-    function_name = ["getRawGyroscopeRate", "getRawAccelerometerData", "getRawCompassData"]
+def generate_table(devices, t_len=10):
+    function_names = ["getRawGyroscopeRate", "getRawAccelerometerData", "getRawCompassData"]
     try:
         # run the recording for t_len seconds
         t_end = time.time() + t_len
         while time.time() < t_end:
-            if mode != 3:
-                line = ""
-                for i, device in enumerate(devices):
-                    # this way we can call a function using its name as a string
-                    function = getattr(device, function_name[mode])
-                    # separate the items with an empty column (separating output from diffrent devices)
-                    if i: line += ",,"
-                    line += "%.03f,%.03f,%.03f" % function()
-                line += ";\n"
-                fp.writelines(line)
-                print(line, end='')
-            else:
-                # this is the mode with all 3 other function
-                for j, func_name in enumerate(function_name):
-                    line = ""
-                    for i, device in enumerate(devices):
-                        function = getattr(device, func_name)
-                        if not i:
-                            line += file_name[j]
-                        line += ",,%.03f,%.03f,%.03f" % function()
-                    line += ";\n"
-                    fp.writelines(line)
-                    print(line, end='')
-                fp.writelines("\n")
+            # this is the mode with all 3 other function
+            row = []
+            for device in devices:
+                for func_name in function_names:
+                    for data in getattr(device, func_name)():
+                        row.append(data)
+            print(row)
 
     except KeyboardInterrupt:
         print("stopped")
 
-    fp.close()
 
 def countdown(seconds):
     for i in reversed(range(seconds)):
-        print(str(i) + "...")
+        print(str(i + 1) + "...")
         time.sleep(1)
     print("GO!")
 
